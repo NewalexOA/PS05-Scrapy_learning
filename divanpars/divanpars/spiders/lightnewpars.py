608a66b9e4dc5e9f4274367205ba73263e4b5aea
Anashkin Aleksei <109337222+NewalexOA@users.noreply.github.com>
2024-05-18 00:00:14 +0300

    add write-to-csv logics

--- divanpars/divanpars/spiders/lightnewpars.py
+++ divanpars/divanpars/spiders/lightnewpars.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import csv
 
 class DivannewparsSpider(scrapy.Spider):
     name = "lightnewpars"
@@ -8,9 +8,16 @@ class DivannewparsSpider(scrapy.Spider):
 
     def parse(self, response):
         lights = response.css("div.lsooF")
+        parsed_data = []
         for light in lights:
-            yield {
+            yield
+            parsed_data.append({
                 "name": light.css('div.lsooF span::text').get(),
                 "url": light.css('a[class="ui-GPFV8 qUioe ProductName ActiveProduct"]').attrib["href"],
                 "price": light.css('span[data-testid="price"]::text').get()
-            }
+            })
+
+        with open("light.csv", "w") as f:
+            writer = csv.DictWriter(f, fieldnames=["name", "url", "price"])
+            writer.writeheader()
+            writer.writerows(parsed_data)
